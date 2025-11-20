aeeb16eb3b12a54625fb9f758bb10e5090cdc7ca
Deng Kuat <denq1738@gmail.com>
2025-11-20 23:52:57 +0200

    Exercise

---
+++ Asynchronous & Event-Based Programming/AsyncAwaitPromiseExercise/1.js
@@ -0,0 +1,24 @@
+async function sequentialWait(){
+  const twoSeconds = await new Promise(resolve => {
+    setTimeout(()=>{
+    console.log(`Hello after 2 seconds`)
+      resolve();
+  }, 2000)
+  });
+  const oneAfter = await new Promise(resolve => {
+    setTimeout(()=>{
+    console.log(`Done`)
+      resolve()
+  }, 1000)
+    
+});
+}                                  
+sequentialWait();
+
+
+/**
+ Write an async function that:
+	•	Waits 2 seconds
+	•	Then logs "Hello after 2 seconds"
+	•	Then waits 1 more second and logs "Done"
+ */
