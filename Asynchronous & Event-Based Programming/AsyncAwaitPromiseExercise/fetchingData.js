74a6839f07c637ae81dc8076f678f7e9269ea203
Deng Kuat <denq1738@gmail.com>
2025-12-04 22:26:37 +0200

    fetching data

--- Asynchronous & Event-Based Programming/AsyncAwaitPromiseExercise/fetchingData.js
+++ Asynchronous & Event-Based Programming/AsyncAwaitPromiseExercise/fetchingData.js
@@ -1,7 +1,7 @@
 async function fetchData() {
   try {
     const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
-    if (!response.ok) throw new Error(response.status);
+    if (!response.ok) throw new Error(response.status); 
     const data = await response.json();
     console.log(data.name);
   }catch(error){
