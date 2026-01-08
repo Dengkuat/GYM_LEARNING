46ac1035fa4a2c1adc978dcd18b52708763dc5b2
Deng Kuat <denq1738@gmail.com>
2026-01-08 16:59:08 +0200

    Boolean Check type

---
+++ TypeScript/Fix The Code TS Errors/boolean check type.ts
@@ -0,0 +1,16 @@
+let isLoggedIn :boolean = true;
+
+if (isLoggedIn) {
+  console.log("User logged in");
+}
+
+or
+
+let isLoggedIn :boolean | string;
+if(isLoggedIn === "true"){
+    isLoggedIn === true;
+}
+
+if(isLoggedIn === true){
+    console.log("User logged in");
+}
