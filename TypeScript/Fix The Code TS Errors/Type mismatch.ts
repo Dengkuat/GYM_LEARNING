699fc56a5717ffff01444dbae35593619287ed42
Deng Kuat <denq1738@gmail.com>
2026-01-09 18:44:20 +0200

    Type Missmatch

---
+++ TypeScript/Fix The Code TS Errors/Type mismatch.ts
@@ -0,0 +1,14 @@
+function add(a: number, b: number) :string {
+  return a + b;
+}
+
+const result: string = add(5, 3);
+console.log(result);
+
+
+//so we have a function that adds 2 numbers a is number and b is number 
+//we donnot have a return type 
+
+//if we just add a and b then it will result in a number 
+//so inorder for the result to work we nned to turn whatever that adds a and b 
+//to a string
