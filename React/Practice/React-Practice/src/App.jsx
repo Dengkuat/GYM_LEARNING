c7a3d53c1cc7d3859807a0c5c09da815d4d3d0f8
Deng Kuat <denq1738@gmail.com>
2026-01-30 23:39:05 +0200

    Validation form(Login)

--- React/Practice/React-Practice/src/App.jsx
+++ React/Practice/React-Practice/src/App.jsx
@@ -1,27 +1,12 @@
-import { ProfileCard } from "./components/ProfileCard";
+import Login from "./components/formValidation";
 
 function App(){
   return (
-    <div>
-      <h1>Introduction</h1>
-      <h1>USER ONE</h1>
-      <ProfileCard
-      name='deng'
-      country='SSD'
-      province='Juba'
-      school='UOK'
-      coure='CS'
-      />
-       <h1>USER TWO</h1>
-      <ProfileCard
-      name='asldofasd'
-      country='ajsodifnqwaed'
-      province='asdogjaners'
-      school='ajsodignr'
-      coure='fasoiudgjbf'
-      />
-    </div>
-  );
+   <div>
+    <h1>Please log in</h1>
+    <Login />
+   </div>
+  )
 }
 
 export default App;
