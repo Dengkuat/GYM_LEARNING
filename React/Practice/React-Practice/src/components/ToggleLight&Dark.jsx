bd2a3365965f07c29f4fd4c8cd718d8ca468c5aa
Deng Kuat <denq1738@gmail.com>
2026-01-30 23:40:04 +0200

    Toggle Light and Dark

---
+++ React/Practice/React-Practice/src/components/ToggleLight&Dark.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+impo
+
+
+function ThemToggle(){
+  const [isDark, SetIsDark] = useState(false);
+
+  return(
+  <div></div>  
+
+  )
+}
+
+export default ThemToggle;
