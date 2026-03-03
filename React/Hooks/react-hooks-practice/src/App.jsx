8f6e0b045fdd990383cb55f1b274998ac3e105e6
Deng Kuat <denq1738@gmail.com>
2026-03-03 09:30:53 +0200

    App.jsx render update

--- React/Hooks/react-hooks-practice/src/App.jsx
+++ React/Hooks/react-hooks-practice/src/App.jsx
@@ -2,11 +2,12 @@
 import Counter from "./components/counter";
 import FunctionalUpdates from "./components/functionalUpdates";
 import Scores from "./components/batching&functionalUpdates&objects";
+import Score from "./components/useReducer";
 
 export default function App(){
   return(
     <div>
-      <Scores/>
+      <Score/>
     </div>
   )
 }
