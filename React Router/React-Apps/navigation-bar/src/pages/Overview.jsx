4e193adf9dfe60e44974f38ab66fe75b8f90ab0b
Deng Kuat <denq1738@gmail.com>
2026-03-03 16:00:14 +0200

    Overview

--- React Router/React-Apps/navigation-bar/src/pages/Overview.jsx
+++ React Router/React-Apps/navigation-bar/src/pages/Overview.jsx
@@ -1,7 +1,4 @@
-export default function Overview(){
-  return(
-    <div>
-      <h1>Overview Page</h1>
-    </div>
-  )
+
+export default function Overview() {
+  return <h1 className="text-2xl">Overview Page</h1>;
 }
