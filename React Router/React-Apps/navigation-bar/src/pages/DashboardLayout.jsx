3780eaeadd5a87d99f55b21816acbbc0853e7429
Deng Kuat <denq1738@gmail.com>
2026-03-03 15:59:57 +0200

    Dashboard Layout

--- React Router/React-Apps/navigation-bar/src/pages/DashboardLayout.jsx
+++ React Router/React-Apps/navigation-bar/src/pages/DashboardLayout.jsx
@@ -1,21 +1,24 @@
-import {Outlet, Link} from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 
-export default function DashboardLayout(){
-  return(
+export default function DashboardLayout() {
+  return (
     <div className="flex h-screen">
-
-      <div className="w-64 bg-gray-800 text-white px-4">
+      
+      {/* Sidebar */}
+      <div className="w-64 bg-gray-800 text-white p-4">
         <h2 className="text-xl mb-6">Dashboard</h2>
         <nav className="flex flex-col space-y-4">
-          <Link to={overview}>Overview</Link>
-          <Link to={profile}>Profile</Link>
-          <Link to={settings}>Setting</Link>
+          <Link to="overview">Overview</Link>
+          <Link to="profile">Profile</Link>
+          <Link to="settings">Settings</Link>
         </nav>
       </div>
 
+      {/* Main Content */}
       <div className="flex-1 p-8 bg-gray-100">
         <Outlet />
       </div>
+
     </div>
-  )
+  );
 }
