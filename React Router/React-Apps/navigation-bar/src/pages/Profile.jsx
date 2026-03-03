1d915de62c701f370e042cf3a872e99da27bfa2c
Deng Kuat <denq1738@gmail.com>
2026-03-03 16:01:52 +0200

    profile navigation

--- React Router/React-Apps/navigation-bar/src/pages/Profile.jsx
+++ React Router/React-Apps/navigation-bar/src/pages/Profile.jsx
@@ -1,7 +1,4 @@
-export default function Profile(){
-  return(
-    <div>
-      <h1>Profile Page</h1>
-    </div>
-  )
+
+export default function Profile() {
+  return <h1 className="text-2xl">Profile Page</h1>;
 }
