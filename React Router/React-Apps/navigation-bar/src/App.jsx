567d39845d41e26915455dba952e36f993029613
Deng Kuat <denq1738@gmail.com>
2026-03-05 16:08:59 +0200

    App

--- React Router/React-Apps/navigation-bar/src/App.jsx
+++ React Router/React-Apps/navigation-bar/src/App.jsx
@@ -1,20 +1,15 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import DashboardLayout from "./pages/DashboardLayout.jsx";
-import Overview from "./pages/Overview.jsx";
-import Profile from "./pages/Profile.jsx";
-import Settings from "./pages/Settings.jsx";
+import { Routes, Route } from "react-router-dom";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/dashboard" element={<DashboardLayout />}>
-        <Route index element={<Navigate to="overview" replace />} /> {/* ← default */}
-        <Route path="overview" element={<Overview />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="settings" element={<Settings />} />
-      </Route>
 
-      <Route path="*" element={<h1>404 — Not Found</h1>} />
+      <Route path="/products" element={<Products />} />
+
+      <Route path="/products/:id" element={<ProductDetails />} />
+
     </Routes>
   );
 }
