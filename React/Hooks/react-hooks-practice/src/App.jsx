9f867080c69fd3814475be6de6d3bfacbd602230
Deng Kuat <denq1738@gmail.com>
2026-03-05 20:46:59 +0200

    App

--- React/Hooks/react-hooks-practice/src/App.jsx
+++ React/Hooks/react-hooks-practice/src/App.jsx
@@ -3,11 +3,12 @@ import Counter from "./components/counter";
 import FunctionalUpdates from "./components/functionalUpdates";
 import Scores from "./components/batching&functionalUpdates&objects";
 import Score from "./components/useReducer";
+import AutoIncrementer from "./components/autoIncrementer useEffect";
 
 export default function App(){
   return(
     <div>
-      <Score/>
+      <AutoIncrementer/>
     </div>
   )
 }
