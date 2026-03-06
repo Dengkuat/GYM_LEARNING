a39498c056456b53ac2c0ec3d31af01c24a83e94
Deng Kuat <denq1738@gmail.com>
2026-03-06 09:20:03 +0200

    Auto incrementer with useEffect

--- React/Hooks/react-hooks-practice/src/components/autoIncrementer useEffect.jsx
+++ React/Hooks/react-hooks-practice/src/components/autoIncrementer useEffect.jsx
@@ -1,32 +1,24 @@
 import { useState, useEffect } from "react";
 
-export default function AutoIncrementer(){
-  console.log(`compoenet mounted`)
+export default function AutoIncrementer() {
 
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCount(count+1);
-    }, 1000)
+      setCount(prev => prev + 1);
+    }, 1000);
 
-    return() => {
-      clearInterval(interval)
-    }
-  }, [count])
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-  return(
+  return (
     <div>
       <button>
-       autoIncrementer after every 1s: {count}
+        Auto Increment every 1s: {count}
       </button>
     </div>
-  )
+  );
 }
-
-// Auto Counter
-
-// Requirements:
-// 	•	start at 0
-// 	•	increase every 1 second
-// 	•	stop when component unmounts
