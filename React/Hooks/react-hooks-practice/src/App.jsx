dcac51c178df14735f8e1d4f0745c88403df5a84
Deng Kuat <denq1738@gmail.com>
2026-03-06 14:10:22 +0200

    App.jsx Router apps

--- React/Hooks/react-hooks-practice/src/App.jsx
+++ React/Hooks/react-hooks-practice/src/App.jsx
@@ -4,11 +4,15 @@ import FunctionalUpdates from "./components/functionalUpdates";
 import Scores from "./components/batching&functionalUpdates&objects";
 import Score from "./components/useReducer";
 import AutoIncrementer from "./components/autoIncrementer useEffect";
+import AutoToggle from "./components/Auto Toggle";
+import CountDownTimer from "./components/countDownTimer";
+import RandomNumberGenerator from "./components/Random Number Generator";
+import APIRolling from "./components/API Polling";
 
 export default function App(){
   return(
     <div>
-      <AutoIncrementer/>
+      <APIRolling/>
     </div>
   )
 }
