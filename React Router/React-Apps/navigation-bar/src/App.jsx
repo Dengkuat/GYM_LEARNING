6311f87414cbf3c984a8e819efae8f0b9d41d9f1
Deng Kuat <denq1738@gmail.com>
2026-03-06 15:27:26 +0200

    React Apps

--- React Router/React-Apps/navigation-bar/src/App.jsx
+++ React Router/React-Apps/navigation-bar/src/App.jsx
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
+import { useSearchParams } from "react-router-dom";
 
 export default function App() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const category = searchParams
   return (
-    <Routes>
-
-      <Route path="/products" element={<Products />} />
-
-      <Route path="/products/:id" element={<ProductDetails />} />
-
-    </Routes>
+    <>
+      
+    </>
   );
 }
