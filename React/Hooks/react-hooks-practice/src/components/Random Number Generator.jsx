cf17483bf578f2d3c74fc7690e5dec5b78a5bdee
Deng Kuat <denq1738@gmail.com>
2026-03-06 10:10:12 +0200

    Random Number Generator

---
+++ React/Hooks/react-hooks-practice/src/components/Random Number Generator.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export default function RandomNumberGenerator(){
+  const [number, setNumber] = useState(0);
+  const [isRunning, setIsRunning] = useState(true); 
+
+  useEffect(() => {
+    if(!isRunning) return
+    const interval = setInterval(() => {
+      setNumber(Math.floor(Math.random() * 101))
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [isRunning])
+  return(
+    <div>
+      <p>Random Number: {number}</p>
+      <button className="border py-2 px-4 rounded-2xl"
+      onClick={() => setIsRunning(false)}
+      >
+        stop Random Generation
+      </button>
+    </div>
+  )
+}
