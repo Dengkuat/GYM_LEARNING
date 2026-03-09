462fefa28e44ebf7c0390351c9b6da98ccafa46d
Deng Kuat <denq1738@gmail.com>
2026-03-09 14:45:04 +0200

    Fetch and Display posts

---
+++ React/Hooks/react-hooks-practice/src/components/fetchAndDisplayPosts.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export default function FetchDisplayPosts() {
+  const [posts, setPosts] = useState([]);
+  
+  async function fetchingData() {
+    try {
+      setLoading(true);
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+      if (!response.ok) throw new Error(`Failed to fetch posts`);
+      const data = await response.json();
+      setPosts(data)
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchingData();
+  }, [])
+  return (
+    <>
+
+    <button onClick={fetchingData} className="border p-3 rounded-2xl">
+      Refetch data
+    </button>
+      <pre>{JSON.stringify(posts, null, 3)}</pre>
+    </>
+  )
+}
