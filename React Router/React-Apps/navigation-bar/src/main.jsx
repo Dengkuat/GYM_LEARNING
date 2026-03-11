75a559425a5ba4b56fe4f04b0b0f24c2b889ab87
Deng Kuat <denq1738@gmail.com>
2026-03-11 15:16:27 +0200

    adding the router provider in my main.jsx

--- React Router/React-Apps/navigation-bar/src/main.jsx
+++ React Router/React-Apps/navigation-bar/src/main.jsx
@@ -1,10 +1,8 @@
-import { BrowserRouter } from "react-router-dom";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { RouterProvider } from "react-router-dom"
+import { router } from "./router"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+  <RouterProvider router={router} />
+)
